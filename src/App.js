66e02bb863d35cd5f65759f3121907d31978f9f9
VIgneshwaran Balamurugan <pvjb187@gmail.com>
2020-05-24 20:44:23 +0530

    app created && homepage init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import HomePage from './HomePage';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
