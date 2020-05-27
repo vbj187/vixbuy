4d0b806c46b31621aca60351d6ca9e8cda7e6eff
VIgneshwaran Balamurugan <pvjb187@gmail.com>
2020-05-27 23:21:16 +0530

    Shop page init

---
+++ src/pages/shop/Shop.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import CollectionPreview from '../../components/preview-collection/CollectionPreview';
+import SHOP_DATA from './shop-data.js';
+
+class Shop extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+
+    return (
+      <div className='shop-page'>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Shop;
